cda72bc48ab1d8d3805c3f007c49cec49dfa1aff
s2pair <samostzz@gmail.com>
2018-06-01 16:38:59 +0700

    test block/allow request on aws waf script first upload

---
+++ aws-waf-test.py
@@ -0,0 +1,65 @@
+import requests
+
+url = 'http://www.url.com' # url or path 
+f = open("payload/payload.txt","r") # open payload file, "r" is read only mode
+countRequests = 0 # variable used to count number of requests/payloads
+countSuccessRequests = 0 # variable used to count success requests (send on uptime and)
+countFailedRequests = 0 # variable used to count fail requests (send on downtime or other fail)
+countAllowRequests = 0 # variable used to count allowed requests
+countBlockedRequests = 0 # variable used to count blocked requests
+coubtErrors = 0 # variable used to count error requests
+for w in f:
+    countRequests+=1
+    try:
+        r = requests.get(url, params=w)
+        countSuccessRequests+=1
+        if r.status_code == 200:
+            countAllowRequests+=1
+        if r.status_code == 403:
+    	    print w #print payloads/requests was blocked by cloudfront
+    	    countBlockedRequests+=1
+        if r.status_code != 200 and r.status_code != 403 :
+            print 'Fail: code='+str(r.status_code)+', Payload: '+w
+    except Exception as e:
+        countFailedRequests+=1
+
+print('########## AWS WAF Testing on '+url+' ##########')
+print('Total requests: '+str(countRequests)+' requests')
+print('Success requests: '+str(countSuccessRequests)+' requests')
+print('Failed requests: '+str(countFailedRequests)+' requests')
+print('Allowed requests: '+str(countAllowRequests)+' requests')
+print('Blocked by cloudfront(AWS WAF): '+str(countBlockedRequests)+' requests')
+print('###################################################################')
+def getResult():
+    print('############ Allow: '+str(countAllowRequests)+' requests, Blocked: '+str(countBlockedRequests)+' requests #############')
+if countSuccessRequests == countRequests:
+    print('####################  All request was success  ####################')
+    getResult()
+elif countFailedRequests == countRequests:
+    print('####################  All request was failed   ####################')
+elif countSuccessRequests < countRequests:
+    print('############ Success: '+str(countSuccessRequests)+' requests, '+'Failed: '+str(countFailedRequests)+' requests ############')
+    getResult()
+else :
+    print('Why success requests are more than total request ?')
+print('###################################################################')
+
+#payload = f.readlines()
+#payload = {'key1': 'value1', 'key2': 'value2'}
+
+# GET
+#r = requests.get(url)
+
+# GET with params in URL
+#r = requests.get(url, params=payload)
+
+# POST with form-encoded data
+#r = requests.post(url, data=payload)
+
+# POST with JSON 
+#import json
+#r = requests.post(url, data=json.dumps(payload))
+
+# Response, status etc
+#r.text
+#print r.status_code
